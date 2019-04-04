95672ade7dff196d6c8a6c111aac9d3d7c6b70a3
Elizabeth Joseph <744248@student.tor.loc>
2019-04-04 10:57:17 -0400

    first commit

---
+++ File2.js
@@ -0,0 +1,16 @@
+
+//Printing numbers upto 11
+console.log("The numbers are :")
+for(var i=1;i<=11;i++)
+{
+    console.log(i);    
+}
+//Addition using while loop
+var sum=0;
+var num=1;
+while(num<=11)
+{
+    sum=sum+num;
+    num++;
+}
+console.log("The sum of numbers is "+sum);
